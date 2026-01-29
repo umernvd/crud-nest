53128afeb293757a013c71b6e0e35797bb6f4dc7
umernvd <muhammadumernaveed1234@gmail.com>
2026-01-29 12:58:07 +0500

    Add class-validator and update CreateUserDto with validation rules

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+}
