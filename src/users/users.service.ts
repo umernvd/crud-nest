8b13d7a54f1b4be4c1b55bff78ade6bd41337902
umernvd <muhammadumernaveed1234@gmail.com>
2026-01-29 12:51:13 +0500

    Refactor UsersService to implement CRUD operations using TypeORM

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  // Create
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
   }
 
+  // Read All
   findAll() {
-    return `This action returns all users`;
+    return this.usersRepository.find();
   }
 
+  // Read One
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.usersRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  // Update
+  async update(id: number, updateUserDto: any) {
+    await this.usersRepository.update(id, updateUserDto);
+    return this.findOne(id);
   }
 
+  // Delete
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.usersRepository.delete(id);
   }
 }
