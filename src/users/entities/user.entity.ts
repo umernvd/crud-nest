2025ee4af4fd0d02f594b9c78b98b5066e3ca3d7
umernvd <muhammadumernaveed1234@gmail.com>
2026-01-29 12:40:47 +0500

    Define User entity with id, name, and email fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+}
